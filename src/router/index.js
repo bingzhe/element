c3ab248f60965290068ff5c862763be117661654
bingzhe <874177815@qq.com>
2017-06-01 19:49:47 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
+import App from '../App'
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 
+// import Hello from '@/components/Hello'
+import ProfilePage from '../page/profile/profile'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      component: App,
+      children: [
+        {
+          path: '/profile',
+          component: ProfilePage
+        }
+
+      ]
     }
   ]
 })
