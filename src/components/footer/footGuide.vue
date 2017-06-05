441148f8c66d456e5ba8297b130957a4f309fb2b
bingzhe <874177815@qq.com>
2017-06-05 21:06:36 +0800

    foot, sass

---
+++ src/components/footer/footGuide.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="foot">
+        111111111111111111
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../../style/mixin';
+
+    #foot{
+        width: 100%;
+        height: 100px;
+        background: $blue;
+    }
+</style>
+
+
--- src/page/profile/profile.vue
+++ src/page/profile/profile.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>11111111111111111</div>
+  <div id="profile-page">
+    11111111111111111
+    <foot-guide></foot-guide>
+  </div>
 </template>
 
 <script>
+import FootGuide from '../../components/footer/footGuide'
 export default {
+  data () {
+    return {
 
+    }
+  },
+  components: {
+    FootGuide
+  }
 }
 </script>
 
