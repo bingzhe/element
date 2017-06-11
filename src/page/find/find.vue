cdf9c5220fe4f4b0a660b0ee11eb72be4fe62fe7
ranzhouli <874177815@qq.com>
2017-06-11 23:49:04 +0800

    修改路由

---
+++ src/page/find/find.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="find-page">
+      find````````````1111111111111
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,19 +5,26 @@ import Router from 'vue-router'
 // import Hello from '@/components/Hello'
 import ProfilePage from '../page/profile/profile'
 import InfoPage from '../page/profile/children/info'
+import FindPage from '../page/find/find'
 Vue.use(Router)
 
 export default new Router({
     routes: [{
         path: '/',
         component: App,
-        children: [{
+        //个人信息页面
+        children: [ //二级路由
+            {
                 path: '/profile',
                 component: ProfilePage,
                 children: [{
                     path: 'info', //个人信息详情页
                     component: InfoPage
                 }]
+            },
+            {
+                path: '/find',
+                component: FindPage
             }
 
         ]
