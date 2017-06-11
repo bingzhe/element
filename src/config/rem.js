9e97100913e7060142172ad43ad4c7d0019c19aa
ranzhouli <874177815@qq.com>
2017-06-12 00:40:15 +0800

    config

---
+++ src/config/rem.js
@@ -0,0 +1,9 @@
+(function(doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
+        };
+})(document, window)
