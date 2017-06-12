bcfe67b652fa8958a751b0edb6b1623c76304f4d
bingzhe <874177815@qq.com>
2017-06-12 18:10:25 +0800

    页面样式

--- src/config/rem.js
+++ src/config/rem.js
@@ -6,4 +6,7 @@
             if (!clientWidth) return;
             docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
         };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window)
