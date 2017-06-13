8c6816c4199e526bb6b080b4841ca18bfa4b5312
ranzhouli <874177815@qq.com>
2017-06-13 23:25:36 +0800

    profile page

--- src/page/profile/profile.vue
+++ src/page/profile/profile.vue
@@ -11,6 +11,30 @@
         <symbol viewBox="0 0 655 1024" id="mobile">
           <path d="M0 122.501v778.998C0 968.946 55.189 1024 123.268 1024h408.824c68.52 0 123.268-54.846 123.268-122.501V122.501C655.36 55.054 600.171 0 532.092 0H123.268C54.748 0 0 54.846 0 122.501zM327.68 942.08c-22.622 0-40.96-18.338-40.96-40.96s18.338-40.96 40.96-40.96 40.96 18.338 40.96 40.96-18.338 40.96-40.96 40.96zM81.92 163.84h491.52V819.2H81.92V163.84z"></path>
         </symbol>
+        <symbol viewBox="0 0 40 40" id="order">
+          <path d="M31.5 3h-23C6 3 4 5.1 4 7.7v24.7C4 34.9 6 37 8.5 37h23c2.5 0 4.5-2.1 4.5-4.7V7.7C36 5.1 34 3 31.5 3zM11.8 28.2c-1.1 0-2-.9-2-2.1 0-1.1.9-2.1 2-2.1s2 .9 2 2.1c0 1.2-.9 2.1-2 2.1zm0-6.1c-1.1 0-2-.9-2-2.1 0-1.1.9-2.1 2-2.1s2 .9 2 2.1c0 1.1-.9 2.1-2 2.1zm0-6.2c-1.1 0-2-.9-2-2.1 0-1.1.9-2.1 2-2.1s2 .9 2 2.1c0 1.2-.9 2.1-2 2.1zm5.1 11.9h13.5v-2.6H16.9v2.6zm0-6.5h13.5v-2.6H16.9v2.6zm0-6.6h13.5v-2.6H16.9v2.6z"></path>
+        </symbol>
+        <symbol viewBox="0 0 40 40" id="point">
+          <path d="M34.6 7.1c0-1.1-1-2.1-2.1-2.1h-24c-1.1 0-2 1-2.1 2.1l-1.6 25C4.6 34.3 6.3 36 8.5 36h24c2.2 0 3.9-1.7 3.7-3.9l-1.6-25zm-5.9 6.1c-.2 4.6-3.7 8.2-8.3 8.2-4.6 0-8.2-3.7-8.4-8.3-.3-.2-.5-.6-.5-1 0-.7.6-1.2 1.3-1.2s1.3.6 1.3 1.2c0 .5-.3.9-.7 1.1.2 3.8 3.2 6.8 7.1 6.8 3.9 0 6.8-3.1 7-6.9-.4-.2-.6-.6-.6-1.1 0-.7.6-1.2 1.3-1.2s1.3.6 1.3 1.2c-.2.6-.4 1-.8 1.2z"></path>
+        </symbol>
+        <symbol viewBox="0 0 40 40" id="vip">
+          <path d="M7 33.4c0 1.1.9 1.6 2 1.6h22c1.1 0 2-.5 2-1.6V31H7v2.4z"></path>
+          <path d="M32.1 14.3c-.6 2.3-2.6 4-5.1 4-2.9 0-5.3-2.3-5.3-5.2v-.2c-.5.2-1 .3-1.6.3-.6 0-1.1-.1-1.7-.3v.2c0 2.9-2.4 5.2-5.3 5.2-2.5 0-4.6-1.7-5.1-4.1-.5.4-1.2.6-1.8.6-.3 0-.5.1-.8 0L7.2 29h26l1.7-14.2c-.3.1-.5.1-.8.1-.8 0-1.5-.2-2-.6z" class="st0"></path>
+          <ellipse cx="20.1" cy="8.2" rx="3.2" ry="3.2"></ellipse>
+          <ellipse cx="6.4" cy="10.5" rx="2.4" ry="2.4"></ellipse>
+          <ellipse cx="33.8" cy="10.5" rx="2.4" ry="2.4"></ellipse>
+        </symbol>
+        <symbol viewBox="0 0 40 40" id="download">
+          <path d="M30 5H10c-2.8 0-5 2.2-5 5v20c0 2.8 2.2 5 5 5h20c2.8 0 5-2.2 5-5V10c0-2.8-2.2-5-5-5zm-3.9 22.7c-.1.2-.3.4-.6.5-4.3 2.8-10.1 1.6-13-2.8-2.8-4.3-1.6-10.2 2.8-13 4.3-2.8 10.2-1.6 13 2.8.2.2.3.5.4.8.1.2 0 .5-.2.7l-8.8 5.7c-.2.2-.6.1-.7-.2l-.5-.7c-.4-.6-.2-1.5.4-1.9l5.6-3.6c.2-.2.3-.5.2-.7l-.1-.1c-2.2-1.8-5.4-2.1-7.9-.4-3.1 2-4 6.1-2 9.2 2 3.1 6.1 4 9.2 2 .6-.4 1.3-.2 1.7.4l.3.7c.1.2.4.5.2.6zm3.1-4.4l-.9.6c-.2.2-.6.1-.7-.2L26.5 22c-.2-.2-.1-.6.2-.7l1.8-1.1c.2-.2.6-.1.7.2l.6.9c.3.6.1 1.5-.6 2z"></path>
+        </symbol>
+        <symbol viewBox="0 0 40 40" id="service">
+          <g id="service_XMLID_1_">
+            <path id="service_XMLID_6_" d="M32.2 9.5c-.2-.7-.1-.7-.4-.9-1.7-1.2-5.3.2-6.7 1.9-.8-3.8-3.8-6.1-4.7-5.9-.9-.2-4 2.1-4.8 5.9-1.3-1.7-5-3.1-6.7-1.9-.1.1-.5.6-.5.7C5.4 20.7 15 24.6 19 25.7v8.7c0 .7.3 1.2 1 1.2s1-.5 1-1.2v-8.6c4-1 14.2-4.8 11.2-16.3z"></path>
+            <path id="service_XMLID_7_" d="M6 25c-.9 0-1.6.7-1.6 1.5.1.8.7 1.5 1.6 1.5 3.1 0 6.5 1.5 8.5 3.7.3.3.7.6 1.1.6.4 0 .7-.1 1-.4.6-.6.6-1.6.1-2.3C14.1 26.8 10 25 6 25z"></path>
+            <path id="service_XMLID_8_" d="M34.6 25c-4 0-8.1 1.9-10.7 4.6-.6.6-.5 1.6.1 2.2.3.3.7.4 1 .4.4 0 .8-.3 1.1-.6 2-2.1 5.3-3.7 8.4-3.7h.1c.8 0 1.5-.7 1.5-1.5 0-.7-.7-1.4-1.5-1.4z"></path>
+          </g>
+          <path d="M0 0h40v40H0z" class="st1"></path>
+        </symbol>
       </defs>
     </svg>
     <section>
@@ -44,6 +68,7 @@
         </router-link>
       </section>
     </section>
+  
     <section class="info-data">
       <ul class="clear">
         <router-link to="/balance" tag="li" class="info-data-link">
@@ -62,6 +87,92 @@
           <span class="info-data-bottom">我的积分</span>
         </router-link>
       </ul>
+    </section>
+  
+    <section class="profile-members">
+      <!--我的订单-->
+      <router-link to="/order" class="profile-members-order">
+        <aside>
+          <svg fill="#4aa5f0">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#order"></use>
+          </svg>
+        </aside>
+        <div class="members-order-mod">
+          <span>我的订单</span>
+          <span class="members-order-arrow">
+            <svg fill="#bbb">
+              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+            </svg>
+          </span>
+        </div>
+      </router-link>
+      <!--积分商城-->
+      <router-link to="/order" class="profile-members-order">
+        <aside>
+          <svg fill="#fc7b53">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#point"></use>
+          </svg>
+        </aside>
+        <div class="members-order-mod">
+          <span>积分商城</span>
+          <span class="members-order-arrow">
+            <svg fill="#bbb">
+              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+            </svg>
+          </span>
+        </div>
+      </router-link>
+      <!--饿了么会员卡-->
+      <router-link to="/vipcard" class="profile-members-order">
+        <aside>
+          <svg fill="#ffc636">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#vip"></use>
+          </svg>
+        </aside>
+        <div class="members-order-mod">
+          <span>饿了么会员卡</span>
+          <span class="members-order-arrow">
+            <svg fill="#bbb">
+              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+            </svg>
+          </span>
+        </div>
+      </router-link>
+    </section>
+  
+    <section class="profile-members">
+      <!--我的订单-->
+      <router-link to="/service" class="profile-members-order">
+        <aside>
+          <svg fill="#4aa5f0">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#service"></use>
+          </svg>
+        </aside>
+        <div class="members-order-mod">
+          <span>服务中心</span>
+          <span class="members-order-arrow">
+            <svg fill="#bbb">
+              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+            </svg>
+          </span>
+        </div>
+      </router-link>
+      <!--积分商城-->
+      <router-link to="/download" class="profile-members-order">
+        <aside>
+          <svg fill="#3cabff">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#download"></use>
+          </svg>
+        </aside>
+        <div class="members-order-mod">
+          <span>下载饿了么APP</span>
+          <span class="members-order-arrow">
+            <svg fill="#bbb">
+              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+            </svg>
+          </span>
+        </div>
+      </router-link>
     </section>
     <foot-guide></foot-guide>
   </div>
@@ -89,11 +200,13 @@ export default {
 
 <style lang="scss" scoped>
 @import '../../style/mixin';
-#profile-page{
-  p, span {
-    font-family: Helvetica Neue,Tahoma,Arial;
+#profile-page {
+  p,
+  span {
+    font-family: Helvetica Neue, Tahoma, Arial;
   }
 }
+
 .profile-number {
   padding-top: 1.95rem;
   .profile-link {
@@ -136,7 +249,7 @@ export default {
         }
       }
 
-      p:nth-child(2){
+      p:nth-child(2) {
         display: flex;
         padding-top: .2rem;
       }
@@ -152,7 +265,24 @@ export default {
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*余额，优惠卷，积分*/
+
 .info-data {
   // width: 100%;
   background: $fc;
@@ -189,8 +319,41 @@ export default {
 
     .info-data-link:nth-child(3) {
       border-right: 0;
-      b{
+      b {
         color: #6ac20b;
+      }
+    }
+  }
+}
+
+// 订单，积分，会员卡
+.profile-members {
+  margin-top: .4rem;
+  background: #fff;
+  .profile-members-order {
+    display: flex;
+    aside {
+      flex: 0 1 1.6rem;
+      margin-top: 0.185rem;
+      @include wh(.7rem, .7rem);
+      svg {
+        @include wh(100%, 100%);
+      }
+    }
+    .members-order-mod {
+      flex: 1;
+      @include sc(.7rem, #333);
+      padding: .433333rem .266667rem .433333rem 0;
+      display: flex;
+      justify-content: space-between;
+      span {
+        display: block;
+      }
+      .members-order-arrow {
+        @include wh(.46667rem, .46667rem);
+        svg {
+          @include wh(100%, 100%);
+        }
       }
     }
   }
